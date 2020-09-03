73c69ad2293b6fab9ee22936947f2cd8e885bb64
root <root@DESKTOP-H6RKTHP.localdomain>
2020-09-03 16:58:03 -0300

    S7-Crear-Componente-Search-Bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Route} from 'react-router-dom';
-import Home from './components/home';
-import Producto from './components/producto';
-import ProducList from './components/ProducList'
-import home from './components/home'
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "./components/home";
+import Producto from "./components/producto";
+import ProducList from "./components/ProducList";
+import home from "./components/home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <h1>HENRY MERCH</h1>
+        <Header />
       </div>
-        <hr></hr>
-      <Route exact path = '/' component ={home}/>
-      <Route exact path = '/Producto' render={() => <Producto price={2000} name={'camisa'} description={'hofsdfsd'}/>}/>
-      <Route exact path = '/ProducList' component={ProducList}/>
+
+      <hr></hr>
+      <Route exact path="/" component={home}></Route>
+      <Route
+        exact
+        path="/Producto"
+        render={() => (
+          <Producto price={2000} name={"camisa"} description={"hofsdfsd"} />
+        )}
+      />
+      <Route exact path="/ProducList" component={ProducList} />
     </div>
   );
 }
