cfb3d91f9ced01816469bc2582f8bdbc9a24809e
Manuel <manuelejdelatorre@gmail.com>
2020-09-03 18:49:23 -0300

    Creamos ruta para buscar producto con keyword requerida

---
+++ api/src/routes/search.js
@@ -0,0 +1,20 @@
+const server = require('express').Router();
+const { Product, Category, prodcat } = require('../db.js');
+const Sequelize = require('sequelize');
+//get keyword
+server.get('/', (req, res) => {
+    const Op = Sequelize.Op;
+    console.log(req.query)
+    Product.findAll({
+        [Op.or]: [{
+            name: { [Op.like]: '%' + req.query.query + '%' }
+        }, {
+            description: { [Op.like]: '%' + req.query.query + '%' }
+        }]
+    })
+        .then((producto) => {
+            res.status(200).json(producto);
+        })
+})
+
+module.exports = server;
