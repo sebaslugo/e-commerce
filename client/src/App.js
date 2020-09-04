718425ab9efb585ab6605e74548917f1bc9a5545
Your Name <you@example.com>
2020-09-04 10:15:03 -0500

    Crud agregar productos

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import React from "react";
 
 import "./App.css";
 import { Route } from "react-router-dom";
-import Home from "./components/home";
 import Producto from "./components/producto";
 import ProducList from "./components/ProducList";
 import home from "./components/home";
 import Header from "./components/Header";
 import 'semantic-ui-css/semantic.min.css';
+import  '@material-ui/core/styles';
+
 
 
 
@@ -20,17 +21,9 @@ function App() {
       <div>
         <Header />
       </div>
-
-      <hr></hr>
       <Route exact path="/" component={home}></Route>
-      <Route
-        exact
-        path="/Producto"
-        render={() => (
-          <Producto price={2000} name={"camisa"} description={"hofsdfsd"} />
-        )}
-      />
-      <Route exact path="/ProducList" component={ProducList} />
+      <Route exact path="/:category" component={home}></Route>
+      <Route exact path="/Admin/products" component={ProducList} />
     </div>
   );
 }
