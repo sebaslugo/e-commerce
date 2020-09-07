e3b2e73978bd22091c8ab95fc1dcb9076d564b4b
unknown <jasper13006@gmail.com>
2020-09-07 12:33:19 -0300

    Mostrar imagenes en localHost:3001/urldeImagen

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 // import all routers;
+const express = require('express')
 const productRouter = require('./product.js');
 const searchRouter = require('./search.js')
+const path = require('path');
 
 
 const router = Router();
@@ -11,5 +13,6 @@ const router = Router();
 // router.use('/auth', authRouter--);
 router.use('/products', productRouter);
 router.use('/search', searchRouter)
+router.use(express.static('images'));
 
 module.exports = router;
