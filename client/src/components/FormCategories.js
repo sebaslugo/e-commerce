bfa68f14dc41ca3af02321ce39c90aedc589af5d
unknown <jasper13006@gmail.com>
2020-09-09 10:20:57 -0300

    Inicial state

--- client/src/components/FormCategories.js
+++ client/src/components/FormCategories.js
@@ -4,6 +4,8 @@ import "@material-ui/icons";
 import "@material-ui/core/styles";
 import "./FormCategories.css"
 // var _ = require('lodash');
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 // estos arrays son los que se traen cuando hacemos pedidos al servidor, hay que borrarlos cuando se haga la conexion
 // const categorias = [{'name':'platos'},{'name':'ropa'}]
@@ -13,14 +15,8 @@ import "./FormCategories.css"
 // }
 // console.log(obj)
 
-const productos = [
-  { name: "ropa" },
-  { name: "tazas" },
-  { name: "gorras" },
-  { name: "tapabocas" },
-  { name: "sudo" },
-  { name: "zapatos" },
-];
+
+
 
 export default function MaterialTableDemo() {
   const [state, setState] = React.useState({
@@ -31,53 +27,80 @@ export default function MaterialTableDemo() {
       //   {title:'Imagenes',field:'imagen'},
       //   {title:'Categories',field:'category', lookup:obj,}
     ],
-    data: productos,
   });
+  const [cat, setCat] = useState()
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/products/category')
+      .then(res => {
+        setCat(res.data)
+      })
+  }, [])
+  const refreshPage = () => {
+    window.location.reload(false)
+  }
+
 
   return (
     <div className="FormCategories">
-    <MaterialTable
-      title="Categories List"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState((prevState) => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
-    />
+      <MaterialTable
+        title="Categories List"
+        columns={state.columns}
+        data={cat}
+        editable={{
+          onRowAdd: (newData) =>
+            new Promise((resolve) => {
+              console.log(newData)
+              setTimeout(() => {
+                resolve();
+                axios({
+                  method: 'post',
+                  url: 'http://localhost:3001/products/category',
+                  data: {
+                    name: newData.name
+                  }
+                }).then(res =>
+                  console.log(res))
+                console.log(newData)
+                refreshPage();
+              }, 600);
+            }),
+
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve) => {
+              setTimeout(() => {
+                resolve();
+                console.log(newData)
+                console.log(oldData)
+                axios({
+                  method: 'PUT',
+                  url: `http://localhost:3001/products/category/${oldData.id}`,
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  data: {
+                    name: newData.name
+                  }
+                }).then(res =>
+                  console.log(res))
+                refreshPage();
+              }, 600);
+            }),
+          onRowDelete: (oldData) =>
+            new Promise((resolve) => {
+              setTimeout(() => {
+                axios({
+                  method: 'DELETE',
+                  url: `http://localhost:3001/products/category/${oldData.id}`,
+                }).then(res => {
+                  console.log(res)
+                  setCat(res.data)
+                })
+                refreshPage();
+              }, 600);
+            }),
+        }}
+      />
     </div>
   );
 }
