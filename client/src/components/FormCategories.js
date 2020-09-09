5d2b4099f50ab9fd0cd182f44ae071bb49b62f09
unknown <jasper13006@gmail.com>
2020-09-09 10:25:41 -0300

    Correcciones

--- client/src/components/FormCategories.js
+++ client/src/components/FormCategories.js
@@ -7,28 +7,14 @@ import "./FormCategories.css"
 import { useEffect, useState } from 'react';
 import axios from 'axios'
 
-// estos arrays son los que se traen cuando hacemos pedidos al servidor, hay que borrarlos cuando se haga la conexion
-// const categorias = [{'name':'platos'},{'name':'ropa'}]
-// const obj = {}
-// for (let index = 0; index < categorias.length; index++) {
-//     obj[categorias[index].name] = categorias[index].name;
-// }
-// console.log(obj)
-
-
-
 
 export default function MaterialTableDemo() {
+  const [cat, setCat] = useState();
   const [state, setState] = React.useState({
     columns: [
       { title: "Name", field: "name" },
-      //   { title: 'Price', field: 'price',type:'numeric' },
-      //    {title:'Description',field:'description'},
-      //   {title:'Imagenes',field:'imagen'},
-      //   {title:'Categories',field:'category', lookup:obj,}
     ],
   });
-  const [cat, setCat] = useState()
   useEffect(() => {
     axios
       .get('http://localhost:3001/products/category')
