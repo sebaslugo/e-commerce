755d7dd475968ca46ec2d9650bfe5456419e0c64
Your Name <you@example.com>
2020-09-08 21:26:35 -0500

    creacion producto

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -11,53 +11,85 @@ import {
 import axios from 'axios';
 
 
-
-const categorias = [{'name':'platos'},{'name':'ropa'}]
-
-function Formulario ({producto}) {
-    const [images, setImages] = useState({
-        columns: [
-          { title: 'image', field: 'name' },  
-        ],
-        data: producto.imagen,
-      });
-  const [state,setState] = useState({'category':[],'imagenes':[],'stock':1})
+function Formulario ({producto,categorias}) {
+    const[itemCategoria,setItemCategoria] = useState([])
+    const[checked,setchecked]=useState(false)
+    const [state,setState] = useState({}) 
 
   useEffect(() => {
+    
     if(producto){
-        setState(producto)
+        axios 
+        .get(`http://localhost:3001/products/${producto.id}`)
+        .then(res => {
+        setState(res.data.producto)
+        setItemCategoria(res.data.categorias)
+        })
+        
     }
-},[])
-    const handleCheck = (categoria) => {
-        if(producto.category){
-            for (let i = 0; i < producto.category.length; i++) {
-                if(categoria.name === producto.category[i].name){
-                    return true;
-                }
-            } 
-        }
-    }
+    
+    },[])
 
-    const handleSubmit = () => {
-        console.log(state)
-        axios.post('http://localhost:3001/products',state)
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
+    const handleCheck = (categoria) => {
+        
+                
+            if(itemCategoria){
+                for (let i = 0; i < itemCategoria.length; i++) {
+                    if(categoria.name === itemCategoria[i].name){
+                       return 'delete'              
+                    }
+                } 
+                return 'add'   
+            }          
+            
+        
+    }
+    
+    handleCheck({'name':'carros'})
+    
+    const handleSubmit = (e) => {
+        var bodyFormData = new FormData();
+        bodyFormData.set('name',state.name);
+        bodyFormData.set('price',state.price);
+        bodyFormData.set('description',state.description);
+        bodyFormData.set('stock',state.stock); 
+        if(state.image){
+            state.image.map((imag)=> {
+                bodyFormData.append('image', imag);
+            })
+        }            
+
+     
+        axios({
+            method: 'post',
+            url: 'http://localhost:3001/products',
+            data: bodyFormData,
+            config: { headers: {'Content-Type': 'multipart/form-data' }}
+            })
+            .then(function (response) {
+                //handle success
+                console.log(response);
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+        
     }
     const handleChange = (e, { value }) => {
         setState ({
             ...state,
             [e.target.name] : value,
-            'stock':'1'
         })
     }
     const handleCategory = (e,{value}) => {
         e.preventDefault();
-        let validate = true
+        console.log(value)
+/*         let validate = true
         let obj = {
             'name':value
         }        
-        let array = state.category.filter((categoria) => {
+        let array = categorias.filter((categoria) => {
             if(categoria.name === value){
                 validate = false;
             }
@@ -77,28 +109,39 @@ function Formulario ({producto}) {
                 ...state,
                 category:array
             })
-        }  
+        }  */ 
     } 
     
     const handleFiles = (e) => {
         // console.log("este es el nombre de la url de la foto" ,e.target.files[0].name)
+        let array = []
+        console.log(e.target.files)
+        for (let i = 0; i < e.target.files.length; i++) {
+            array.push(e.target.files[i])   
+        }
+        console.log(array)
         setState({
             ...state,
-            image:e.target.files
-        })     
+            image:array
+        })  
+          
+        /* axios.post('http://localhost:3001/products',e.target.files)
+        .then(res => alert('producto cargado'))
+        .catch(err => alert('completa todos los campos')) */
+        
         /* now you can work with the file list */
-        console.log(state)
-      }
+        
+        }
 
     const handleDelete = (e) => {
-        // console.log(e.target.value)
-        // console.log(state.imagen)
+        
+        
         setState ({
             ...state,
-            // imagenes:state.imagen.filter(imag => imag !== e.target.value)
-            imagenes: e.target.value
+            imagenes:state.imagenes.filter(imag => imag !== e.target.value)
+            /* imagenes: e.target.value */
         })
-        console.log(state)
+        
     }
     
     return (
@@ -124,13 +167,27 @@ function Formulario ({producto}) {
                 onChange={handleChange}
                 
             />
+            <Form.Field
+                control={Input}
+                type='number'
+                label='stock'
+                placeholder='stock'
+                name = 'stock'
+                placeholder= {producto.stock}
+                onChange={handleChange}
+                
+            />
             </Form.Group>
-            <Form.Group inline>
-                <label>Categorias</label>
-                {categorias.map((categoria) => 
-                    <Checkbox label={categoria.name} name='category' defaultChecked ={handleCheck(categoria)} onChange = {handleCategory} value = {categoria.name}  />
-                )}        
-            </Form.Group>
+            <Form.Field>
+                <ul >
+                    {categorias.map((categoria) => 
+                        <li key = {categoria.id}>
+                            {categoria.name}
+                            <button onClick = {handleCategory}>{handleCheck(categoria)}</button>
+                        </li>                        
+                    )}
+                </ul>  
+            </Form.Field>
             <Form.Field
             control={TextArea}
             label='Description'
@@ -140,26 +197,28 @@ function Formulario ({producto}) {
             onChange={handleChange}
 
             />
-            {producto.imagenes && <Form.Field>
+            {state.imagenes && <Form.Field>
                     <label>imagenes</label>
-                    <ul>
+                    <ul >
                     {state.imagenes.map((image) => 
-                        <li>
+                        <li key = {state.id}>
                             {image}
                             <button value = {image} onClick = {handleDelete}>x</button>
                         </li>
                         
                     )}
-
-                    </ul>
-                    
+                    </ul>                    
                 
                 </Form.Field>}
+            
+
+
             <Form.Field>
                 <label>Agregar imagenes</label>
                 <input  className = 'form-imagen' type='file' multiple={true} name='imagen'  accept="image/*" onChange = {handleFiles}></input>
             </Form.Field>
             <Form.Field control={Button} onClick = {handleSubmit}>{'AGREGAR'}</Form.Field>
+            {/* <Form.Field control={Button} type="submit">{'AGREGAR'}</Form.Field> */}
             
             </Form>
             
