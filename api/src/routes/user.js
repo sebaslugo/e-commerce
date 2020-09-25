7caf18b8a36ae465b1e9133b20e93c3ff4454f6f
Manuel <manuelejdelatorre@gmail.com>
2020-09-25 19:15:14 -0300

    Templates de email ya funcionando

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -6,6 +6,10 @@ const bcrypt = require('bcrypt');
 const authentication = require('../jwt');
 const nodemailer = require('nodemailer');
 const isAdmin = require('../middlewares/isAdmin');
+const hbs = require('nodemailer-handlebars');
+var inlineCss = require('nodemailer-juice');
+const ejs = require("ejs");
+
 
 
 /* ------------------------------------------------------------------------------- */
@@ -26,38 +30,47 @@ server.post('/', async (req, res) => {
             .then((user) => {
 
                 var smtpTransport = nodemailer.createTransport({
-                    service: 'gmail',
+                    host: 'smtp.gmail.com',
                     auth: {
                         user: 'ecomerce0410@gmail.com',
                         pass: "henry1234."
                     }
                 });
-                var mailOptions = {
-                    to: email,
-                    from: 'ecomerce0410@gmail.com',
-                    subject: `Hola ${user.name}`,
-                    text: 'Usted se ha registrado correctamente en Henry Store!\n\n' +
-                        'http://' + "localhost:3000" + '\n\n'
-                };
-                smtpTransport.sendMail(mailOptions, function (err, done) {
-                    /* req.flash('success', 'An e-mail has been sent to ' + email + ' with further instructions.'); */
-                    done(err, 'done');
-                });
-                return (user)
 
-            })
-            .then(user => {
-                console.log(User)
-                return res.status(201).json(user)
-            })
-            .catch(error => {
-                console.log(error)
-                return res.status(400).send(error)
+                smtpTransport.verify((error, success) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Server is ready to take messages');
+                    }
+                });
+                ejs.renderFile(__dirname + "/NewUser.ejs", { name: name }, function (err, data) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        var mainOptions = {
+                            to: email,
+                            from: 'ecomerce0410@gmail.com',
+                            subject: `Hola ${user.name}`,
+                            html: data,
+                        };
+                        smtpTransport.sendMail(mainOptions, function (err, info) {
+                            if (err) {
+                                res.json({
+                                    msg: 'fail'
+                                })
+                            } else {
+                                res.json({
+                                    msg: 'success'
+                                })
+                            }
+                        });
+                    }
+                })
             })
     }
+});
 
-
-})
 /* ------------------------------------------------------------------------------- */
 /* S35 : Crear Ruta para modificar Usuario */
 /* ------------------------------------------------------------------------------- */
@@ -152,7 +165,7 @@ server
                 return order.setUser(userId)
             })
             .then((order) => {
-                
+
                 return OrderList.create({
                     price,
                     quantity,
@@ -256,19 +269,22 @@ server
         const { productId } = req.params
         OrderList.destroy({
             where: { orderId: orderId, productId: productId }
-        }).then(respon =>  res.status(200).json(respon))
+        }).then(respon => res.status(200).json(respon))
             .catch(err => res.send(err))
     })
+
+
 
 server
     .route("/forgot")
     .post((req, res, next) => {
+        let name;
         const { email } = req.body
         User.findOne({
             where: { email: email }
 
         }).then((user) => {
-
+            name = user.name;
             let payload = { id: user.id }
             let token = authentication.jwt.sign(payload, authentication.jwtOptions.secretOrKey)
             user.passwordToken = token;
@@ -278,32 +294,51 @@ server
 
         })
             .then((token) => {
-
                 var smtpTransport = nodemailer.createTransport({
-                    service: 'gmail',
+                    host: "smtp.gmail.com",
                     auth: {
                         user: 'ecomerce0410@gmail.com',
                         pass: "henry1234."
                     }
                 });
-                var mailOptions = {
-                    to: email,
-                    from: 'ecomerce0410@gmail.com',
-                    subject: 'Solicitud de cambio de contraseña',
-                    text: 'Recibio este correo porque usted u otra persona ha solicitado el restablecimiento de contraseña de su cuenta, para restablecer, dirigase al siguiente link :\n\n' +
-                        "http://localhost:3000/login/changepass/" + token + '\n\n' +
-                        'Si usted no solicito un cambio de contraseña, haga caso omiso a este mensaje.\n'
-                };
-                smtpTransport.sendMail(mailOptions, function (err) {
-                    done(err, 'done');
-                });
-                return res.status(200).json({ "token": token })
 
-            },
-                function (err) {
-                    if (err) return next(err);
+                smtpTransport.verify((error, success) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Server is ready to take messages');
+                    }
                 });
-    })
+
+                ejs.renderFile(__dirname + "/ChangePassword.ejs", { name: name, token: token }, function (err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        var mainOptions = {
+                            from: 'ecomerce0410@gmail.com',
+                            to: email,
+                            subject: 'Solicitud de cambio de contraseña',
+                            html: data
+                        };
+                        //console.log("html data ======================>", mainOptions.html);
+
+                        smtpTransport.sendMail(mainOptions, function (err, info) {
+                            if (err) {
+                                res.json({
+                                    msg: 'fail'
+                                })
+                            } else {
+                                res.json({
+                                    msg: 'success'
+                                })
+                            }
+                        });
+                    }
+                })
+
+
+            })
+    });
 /* ------------------------------------------------------------------------------- */
 //  Ruta para cmabiar contraseña
 /* ------------------------------------------------------------------------------- */
