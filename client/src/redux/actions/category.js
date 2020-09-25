c331cd91b879a4cff81d6b3f6790b94504c3639d
unknown <jasper13006@gmail.com>
2020-09-25 19:41:17 -0300

    se añadio en category

--- client/src/redux/actions/category.js
+++ client/src/redux/actions/category.js
@@ -3,32 +3,32 @@
 /* ------------------------------------------------------------------------------- */
 import axios from 'axios';
 import { GET_CATEGORY, DELETE_CATEGORY, CREATE_CATEGORY, EDIT_CATEGORY } from '../consts/actionTypes';
-
+import Swal from 'sweetalert2'
 
 export function getCategories() {
-    return function(dispatch) {
+    return function (dispatch) {
         return axios.get("http://localhost:3001/products/category")
-          .then(response =>{
-            dispatch({ type: GET_CATEGORY, payload: response.data });
-          })
-          
+            .then(response => {
+                dispatch({ type: GET_CATEGORY, payload: response.data });
+            })
+
     };
 
 }
 
 export function deleteCategory(oldData) {
-    return function(dispatch) {
+    return function (dispatch) {
         return axios.delete(`http://localhost:3001/products/category/${oldData.id}`)
-          .then(response =>{
-            dispatch({ type: DELETE_CATEGORY, payload: response.data });
-          })
-          
+            .then(response => {
+                dispatch({ type: DELETE_CATEGORY, payload: response.data });
+            })
+
     };
 }
 
 
 export function postCategories(newData) {
-    return function(dispatch) {
+    return function (dispatch) {
         return axios({
             method: 'POST',
             url: "http://localhost:3001/products/category",
@@ -36,21 +36,21 @@ export function postCategories(newData) {
                 name: newData.name
             }
         })
-        .then(res => {
-            dispatch({
-                type: CREATE_CATEGORY,
-                payload: res.data.name
-            })
-        }
-        )
-        .catch(err => console.error(err.message));
-          
+            .then(res => {
+                dispatch({
+                    type: CREATE_CATEGORY,
+                    payload: res.data.name
+                })
+            }
+            )
+            .catch(err => console.error(err.message));
+
     };
-   
+
 }
 
 export function putCategories(newData, oldData) {
-    return function(dispatch) {
+    return function (dispatch) {
         return axios({
             method: 'PUT',
             url: `http://localhost:3001/products/category/${newData.id}`,
@@ -58,45 +58,65 @@ export function putCategories(newData, oldData) {
                 name: newData.name
             }
         })
-        .then(res => {
-            dispatch({
-                type: EDIT_CATEGORY,
-                payload: res.data.name
-            })            
-        })
-        .catch(err => console.error(err.message));
-        };   
-}
-
-export function deleteProductCategory (productId,categoryId) {
-    return function(dispatch) {
-        return axios
-        .delete(`http://localhost:3001/products/${productId}/category/${categoryId}`)
-        .then(function (response) {
-            //handle success
-            alert('se elimino el producto de la categoria')
-        })
-        .catch(function (response) {
-            //handle error
-            alert('ups,intenta de nuevo')
-        }); 
-          
+            .then(res => {
+                dispatch({
+                    type: EDIT_CATEGORY,
+                    payload: res.data.name
+                })
+            })
+            .catch(err => console.error(err.message));
     };
 }
 
-export function addProductCategory (productId,categoryId) {
-    return function(dispatch) {
+export function deleteProductCategory(productId, categoryId) {
+    return function (dispatch) {
         return axios
-        .post(`http://localhost:3001/products/${productId}/category/${categoryId}`)
-        .then(function (response) {
-            //handle success
-            alert('se agrego el producto a la categoria')
-        })
-        .catch(function (response) {
-            //handle error
-            alert(response)
-        }); 
-          
+            .delete(`http://localhost:3001/products/${productId}/category/${categoryId}`)
+            .then(function (response) {
+                //handle success
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Se elimino el producto de la categoria',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            })
+            .catch(function (response) {
+                //handle error
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'intenta de nuevo',
+                })
+            });
+
+    };
+}
+
+export function addProductCategory(productId, categoryId) {
+    return function (dispatch) {
+        return axios
+            .post(`http://localhost:3001/products/${productId}/category/${categoryId}`)
+            .then(function (response) {
+                //handle success
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Se agrego el producto a la categoria',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            })
+            .catch(function (response) {
+                //handle error
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: `${response}`,
+                })
+            });
+
     };
 }
 
