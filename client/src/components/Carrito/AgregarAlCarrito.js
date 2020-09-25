e276fad133969fa06983d7ee38c5129f6a4efa5b
unknown <jasper13006@gmail.com>
2020-09-25 20:35:26 -0300

    agregaralcarrito

--- client/src/components/Carrito/AgregarAlCarrito.js
+++ client/src/components/Carrito/AgregarAlCarrito.js
@@ -1,79 +1,95 @@
 import React, { useEffect } from 'react';
 import IconButton from '@material-ui/core/IconButton';
-import { Button,Icon } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react'
 import agregarAlCarrito from '../../redux/actions/agregarAlCarrito'
 import './AgregarCart.css'
 
+import Swal from 'sweetalert2'
 
-
-export default function AgregarAlCarrito({ producto, precio, cantidad,active }) {
+export default function AgregarAlCarrito({ producto, precio, cantidad, active }) {
     const dispatch = useDispatch();
-    const [id,setId] = useState()
+    const [id, setId] = useState()
     // const content = useSelector(state => state)
-    useEffect(()=>{
-        if(!id){
+    useEffect(() => {
+        if (!id) {
             setId(localStorage.getItem('idUser'))
         }
     })
-    
+
     const enviarDatos = (event) => {
         let carrito = []
-        
-        if(id){  
+
+        if (id) {
             let product = {
-                productId:producto.id,
-                price:parseInt(precio),
-                quantity:parseInt(cantidad)
-            }          
-            dispatch(agregarAlCarrito(product,id))
-            alert('producto agregado al carrito')
-            
+                productId: producto.id,
+                price: parseInt(precio),
+                quantity: parseInt(cantidad)
+            }
+            dispatch(agregarAlCarrito(product, id))
+            Swal.fire({
+                icon: 'success',
+                title: 'Al Carrito',
+                text: 'Producto agregado al carrito exitosamente',
+            })
+
         }
-        else{
-            let product= {products:[producto],orderList:[{productId:producto.id,price:parseInt(precio),quantity:parseInt(cantidad)}]};
-            let local =  JSON.parse(localStorage.getItem("carrito"));  
-            
-            
-            if(local){
-                local.products.map((producto)=>{
-                    if(producto.id == product.products[0].id){
+        else {
+            let product = { products: [producto], orderList: [{ productId: producto.id, price: parseInt(precio), quantity: parseInt(cantidad) }] };
+            let local = JSON.parse(localStorage.getItem("carrito"));
+
+
+            if (local) {
+                local.products.map((producto) => {
+                    if (producto.id == product.products[0].id) {
                         console.log('aqui toy')
                         product = false;
-                        return alert('producto ya esta en el carrito')
+                        return Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            text: 'El producto ya esta en el carrito',
+                        })
                     }
-                }) 
-                if(product){
+                })
+                if (product) {
                     local = {
-                        ['products']:local.products.concat(product.products),
-                        ['orderList']:local.orderList.concat(product.orderList)
+                        ['products']: local.products.concat(product.products),
+                        ['orderList']: local.orderList.concat(product.orderList)
                     }
-                    alert('producto agregado al carrito')
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Al Carrito',
+                        text: 'Producto agregado al carrito exitosamente',
+                    })
                 }
-                
-            } 
-            else{
-                local=product
-                alert('producto agregado al carrito')          
-            } 
-            
-            const serializedState = JSON.stringify(local);
-            localStorage.setItem("carrito", serializedState);   
 
-                        
-        }             
+            }
+            else {
+                local = product
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Al Carrito',
+                    text: 'Producto agregado al carrito exitosamente',
+                })
+            }
+
+            const serializedState = JSON.stringify(local);
+            localStorage.setItem("carrito", serializedState);
+
+
+        }
     }
     return (
         <div>
-            {active ? <Button id = 'cart_Boton'  onClick={() => (enviarDatos())}>
-                <Icon name='add to cart'/>
-                Al Carrito                
+            {active ? <Button id='cart_Boton' onClick={() => (enviarDatos())}>
+                <Icon name='add to cart' />
+                Al Carrito
             </Button> :
-            <a color="primary" aria-label="Añadir al carrito" className="addCarrito"
-                onClick={() => (enviarDatos())}
-            >    Al Carrito
+                <a color="primary" aria-label="Añadir al carrito" className="addCarrito"
+                    onClick={() => (enviarDatos())}
+                >    Al Carrito
             </a>}
         </div>
     );
